1106d919333c6c8a591736eacb6d30ea425e66be
Ivan Fedosov <pro100chok007@gmail.com>
2019-03-22 10:23:04 +0300

    version 1

---
+++ CdProjectRed/src/CdProjectRed.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : CdProjectRed.c
+ Author      : =^_^=
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char string [10];
+
+
+
+int main (void) {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+    puts("123");
+
+}
