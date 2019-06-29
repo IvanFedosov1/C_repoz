6a78cff548adc447a6a0d10310e1d1fc45c5fac7
IvanFedosov1 <47918360+IvanFedosov1@users.noreply.github.com>
2019-06-29 23:09:44 +0300

    Add files via upload

---
+++ calc_6_test/cmake-build-debug/CMakeFiles/calc_6_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_6_test.dir/main.c.obj"
+  "calc_6_test.pdb"
+  "calc_6_test.exe"
+  "calc_6_test.exe.manifest"
+  "libcalc_6_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc_6_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
